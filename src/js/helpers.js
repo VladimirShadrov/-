241bac87777f0c63e569383f461ed9a118586f37
Vladimir <shadrov_vladimir@mail.ru>
2021-06-10 23:14:37 +0300

    Добавлен функционал динамического создания вложенных списков

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,3 +1,5 @@
+import { data } from './data';
+
 export function getDomItem(selector) {
   const item = document.querySelector(selector);
   return item;
@@ -7,7 +9,6 @@ export function createDropdownItem(data, parentElement) {
   const html = `
   <div class="list__item-wrapper">
     <div class="list__item" data-id="${data.id}" data-name="dropdown">
-      <!-- Для разворачивания списка добавить класс list__selector-active -->
       <div class="list__selector"></div>
       <p class="list__item-title">${data.name}</p>
     </div>
@@ -16,7 +17,18 @@ export function createDropdownItem(data, parentElement) {
   `;
 
   parentElement.firstElementChild.insertAdjacentHTML('beforeend', html);
+  return html;
+}
 
+export function createInsideDropdownItem(data, placeToInsert) {
+  const html = `
+  <div class="list__inside-list-item" data-name="inside-dropdown" data-id="${data.id}">
+    <div class="list__inside-selector"></div>
+    <p class="list__inside-item-title">${data.name}</p>
+  </div>
+  `;
+
+  placeToInsert.insertAdjacentHTML('beforeend', html);
   return html;
 }
 
@@ -33,11 +45,37 @@ export function createCheckboxItemSingle(data, parentElement) {
   parentElement.firstElementChild.insertAdjacentHTML('beforeend', html);
 }
 
+export function createCheckbox(data, placeToInsert) {
+  const html = `
+    <div class="list__checkbox-item" data-id="${data.id}" data-name="checkbox">
+      <div class="list__checkbox"></div>
+      <p class="list__checkbox-title">${data.name}</p>
+    </div>
+  `;
+
+  placeToInsert.insertAdjacentHTML('beforeend', html);
+}
+
+export function createInsideCheckboxItem(data, placeToInsert) {
+  let html = '';
+
+  data.forEach((item) => {
+    html += `
+    <div class="list__inside-checkbox-item" data-id="${item.id}" data-name="inside-checkbox">
+      <div class="list__inside-checkbox"></div>
+      <p class="list__checkbox-title">${item.name}</p>
+    </div>
+    `;
+  });
+
+  placeToInsert.insertAdjacentHTML('afterbegin', html);
+}
+
 export function changeListItemImage(targetItem, className) {
   targetItem.firstElementChild.classList.toggle(className);
 }
 
-export function openDropdown(targetItem, data, className) {
+export function openAndCloseDropdown(targetItem, data, className) {
   const targetData = data.find((item) => item.id === +targetItem.dataset.id);
 
   if (
@@ -49,11 +87,63 @@ export function openDropdown(targetItem, data, className) {
     targetItem.nextElementSibling.classList.add('hide');
   }
 
-  // if (
-  //   targetData.groups[0] === 'empty' &&
-  //   targetItem.firstElementChild.classList.contains(className)
-  // ) {
-  //   targetItem.nextElementSibling.classList.add('hide');
-  //   console.log(targetItem);
-  // }
+  if (
+    targetData.groups[0] !== 'empty' &&
+    !targetItem.firstElementChild.classList.contains(className)
+  ) {
+    const div = document.createElement('div');
+    targetItem.parentNode.insertAdjacentElement('beforeend', div);
+    div.classList.add('template-container');
+
+    let wrapper = getDomItem('.template-container');
+
+    createList(
+      targetData.groups,
+      wrapper,
+      createInsideDropdownItem,
+      createCheckbox
+    );
+  }
+
+  if (
+    targetData.groups[0] !== 'empty' &&
+    targetItem.firstElementChild.classList.contains(className)
+  ) {
+    targetItem.nextElementSibling.nextElementSibling.remove();
+  }
+}
+
+export function createList(data, placeToInsert, createDropdown, createChekbox) {
+  data.forEach((dataItem) => {
+    if (dataItem.groups.length) {
+      createDropdown(dataItem, placeToInsert);
+    } else {
+      createChekbox(dataItem, placeToInsert);
+    }
+  });
+}
+
+export function openAndCloseNestedDropdown(targetItem, data, className) {
+  const objectWidthData = data.filter((item) => {
+    if (item.groups.length && item.groups[0] !== 'empty') {
+      return item;
+    }
+  });
+
+  const dataFromObject = objectWidthData[0].groups;
+  const currentData = dataFromObject.find(
+    (item) => item.id === +targetItem.dataset.id
+  );
+
+  const div = document.createElement('div');
+  div.classList.add('list__inside-container');
+
+  if (!targetItem.firstElementChild.classList.contains(className)) {
+    targetItem.insertAdjacentElement('afterend', div);
+    createInsideCheckboxItem(currentData.groups, targetItem.nextElementSibling);
+    targetItem.firstElementChild.classList.add(className);
+  } else {
+    targetItem.firstElementChild.classList.remove(className);
+    targetItem.nextElementSibling.remove();
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,13 @@ import '../styles/header.scss';
 import '../styles/list.scss';
 import '../styles/footer.scss';
 import { data } from './data';
+import { createList } from './helpers';
 import { getDomItem } from './helpers';
 import { createDropdownItem } from './helpers';
 import { createCheckboxItemSingle } from './helpers';
 import { changeListItemImage } from './helpers';
-import { openDropdown } from './helpers';
+import { openAndCloseDropdown } from './helpers';
+import { openAndCloseNestedDropdown } from './helpers';
 
 const list = getDomItem('.list');
 
@@ -16,27 +18,30 @@ require.context('../images', true, /\.(png|jpg|svg|gif)$/);
 require.context('../fonts', true, /\.(ttf|woff|woff2)$/);
 
 // Отрисовка элементов списка из массива данных
-function createList(data, list) {
-  data.forEach((dataItem) => {
-    if (dataItem.groups.length) {
-      createDropdownItem(dataItem, list);
-    } else {
-      createCheckboxItemSingle(dataItem, list);
-    }
-  });
-}
 
-createList(data, list);
+createList(data, list, createDropdownItem, createCheckboxItemSingle);
 
 document.body.addEventListener('click', (event) => {
   event.preventDefault();
 
   if (event.target.dataset.name === 'dropdown') {
-    openDropdown(event.target, data, 'list__selector-active');
+    openAndCloseDropdown(event.target, data, 'list__selector-active');
     changeListItemImage(event.target, 'list__selector-active');
   }
 
   if (event.target.dataset.name === 'checkbox') {
     changeListItemImage(event.target, 'list__checkbox-checked');
+  }
+
+  if (event.target.dataset.name === 'inside-checkbox') {
+    changeListItemImage(event.target, 'list__inside-checkbox-checked');
+  }
+
+  if (event.target.dataset.name === 'inside-dropdown') {
+    openAndCloseNestedDropdown(
+      event.target,
+      data,
+      'list__inside-selector-active'
+    );
   }
 });
